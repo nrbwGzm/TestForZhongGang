85620718dca4d4bf505951dfcb1a0d2c2b59a7c8
nrbwGzm <nrbw@Foxmail.com>
2020-07-12 00:10:10 +0800

    第一次clone后修改代码再push

--- git/src/main/java/test.java
+++ git/src/main/java/test.java
@@ -3,7 +3,10 @@ public class test {
         System.out.println("第一次提交代码");
         System.out.println("第二次提交代码");
         System.out.println("第一次修改代码");
+
         System.out.println("第一次clone后修改代码再push");
         System.out.println("云端修改1");
+        System.out.println("本地同步云端修改后再push");
+
     }
 }
