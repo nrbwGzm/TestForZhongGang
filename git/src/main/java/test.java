3afe5a589eb5a46fcb4943b2b814f5807e9cc5e7
nrbwGzm <40880791+nrbwGzm@users.noreply.github.com>
2020-07-12 00:03:32 +0800

    Update test.java

--- git/src/main/java/test.java
+++ git/src/main/java/test.java
@@ -4,6 +4,6 @@ public class test {
         System.out.println("第二次提交代码");
         System.out.println("第一次修改代码");
         System.out.println("第一次clone后修改代码再push");
-        System.out.println("云端修改");
+        System.out.println("云端修改1");
     }
 }
